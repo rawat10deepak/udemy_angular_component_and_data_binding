20233354eaa5d1a34a2c93284c3031ab06681b09
rawat10deepak <109275183+rawat10deepak@users.noreply.github.com>
2023-01-03 16:59:01 +0530

    life cycle hooks

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,14 +1,70 @@
-import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  DoCheck,
+  Input,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  SimpleChanges,
+  ViewEncapsulation,
+} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css'],
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent
+  implements
+    OnInit,
+    OnChanges,
+    DoCheck,
+    AfterContentChecked,
+    AfterContentInit,
+    AfterViewChecked,
+    AfterViewInit,
+    OnDestroy
+{
   @Input('srvElement') element: { type: string; name: string; content: string };
 
-  constructor() {}
+  constructor() {
+    console.log('constructor called ');
+  }
 
-  ngOnInit() {}
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('ngOnChanges called ');
+    // console.log(JSON.stringify(changes));
+  }
+
+  ngDoCheck() {
+    console.log('ngDoCheck called ');
+  }
+
+  ngAfterContentChecked() {
+    console.log('ngAfterContentChecked called ');
+  }
+
+  ngAfterContentInit() {
+    console.log('ngAfterContentInit called ');
+  }
+
+  ngAfterViewChecked() {
+    console.log('ngAfterViewChecked called ');
+  }
+
+  ngAfterViewInit() {
+    console.log('ngAfterViewInit called ');
+  }
+
+  ngOnInit() {
+    console.log('ngOnInit called ');
+  }
+
+  ngOnDestroy() {
+    console.log('ngOnDestroy called ');
+  }
 }
